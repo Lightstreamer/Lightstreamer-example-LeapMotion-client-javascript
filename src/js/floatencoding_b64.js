be9b4133eab90c9bf1a9f39886111df8a4b333b9
mone <simone.fabiano@gmail.com>
2013-12-19 11:07:24 +0100

    add a TODO

--- src/js/floatencoding_b64.js
+++ src/js/floatencoding_b64.js
@@ -13,6 +13,8 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+
+//TODO make it a module, get rid of unused code
    
 
 //////////////// Base 64 encoding
