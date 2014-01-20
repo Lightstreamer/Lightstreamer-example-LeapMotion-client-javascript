0b701a9a0ff2891ec4bf4719870b59b44aa9bcdf
mone <simone.fabiano@gmail.com>
2014-01-20 12:40:37 +0100

    fix call

--- src/js/LeapMotion.js
+++ src/js/LeapMotion.js
@@ -123,7 +123,7 @@ define(["Inheritance","EventDispatcher","./Constants"],
       onFrame: function(frame) {
         var hand = this.getHandInUse(frame);
         if (!hand) {
-          this.setFist(false); //remove the hand == remove the fist
+          this.setFist(null,false); //remove the hand == remove the fist
           return;
         }
         this.setFist(hand,hand.fingers.length <= FINGERS_OF_FIST);
