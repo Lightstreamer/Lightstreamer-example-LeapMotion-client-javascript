8ab04c3a9671f9268b0306f4f618dc8f3daa53e9
mone <simone.fabiano@gmail.com>
2014-03-31 12:20:39 +0200

    add r.js build conf file

---
+++ build_r.js/app.build.js
@@ -0,0 +1,37 @@
+({
+    appDir: "../src",
+    baseUrl: ".",
+    dir: "../built",
+    
+    //if using node to build the project this setting must be changed to "uglify"
+    optimize: "closure",
+    closure: {
+        CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+        loggingLevel: 'WARNING',
+    },
+    
+    skipDirOptimize: true,
+    
+    normalizeDirDefines: "skip",
+    
+    optimizeCss: "standard.keepLines",
+    
+    //if using the non-namespaced version of the Lightstreamer library remove the "Lightstreamer/" prefix from LightstreamerClient, Subscription and StatusWidget module names
+    paths: {
+      "LightstreamerClient": "empty:",
+      "Subscription": "empty:",
+      "Executor": "empty:",
+      "Inheritance": "empty:",
+      "EventDispatcher": "empty:",
+      "StatusWidget": "empty:"
+    },
+    
+    modules: [
+        {
+            name: "index",
+            includeRequire: false
+        }
+    ],
+    
+    preserveLicenseComments: false
+})
